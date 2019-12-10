c21275b11b4d383d5a34cf4dddd29dcb2b161d72
kkoon9 <rndrnjs2003@naver.com>
2019-12-10 19:32:01 +0900

    Upload Assignment [2] - controllers

---
+++ controllers/groupController.js
@@ -0,0 +1,45 @@
+const GroupService = require('../services/groupsService');
+const rm = require('../module/util/responseMessage');
+const utils = require('../module/util/utils');
+const sc = require('../module/util/statusCode');
+
+module.exports = {
+    readAll: async (req, res) => {
+        GroupService.readAll()
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        });
+    },
+    read: async (req, res) => {
+        const groupIdx = req.params.groupIdx;
+        if(!groupIdx) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        GroupService.read(groupIdx)
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        })
+    },
+    mix: async (req, res) => {
+        GroupService.mix()
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        })
+    },
+}
