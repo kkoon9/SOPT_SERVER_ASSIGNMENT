0a74bc78c6d619fcd5b9c31de32af5b1c8861bb8
kkoon9 <rndrnjs2003@naver.com>
2019-12-10 19:32:28 +0900

    Upload util
    responseMessage, statusCode, utils

---
+++ module/util/responseMessage.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  /* Group */
+  MEMBER_READ_ALL_SUCCESS: "멤버 조회 성공",
+  MEMBER_READ_ALL_FAIL: "멤버 조회 실패",
+  GROUP_READ_SUCCESS: "그룹 조회 성공",
+  GROUP_READ_FAIL: "그룹 조회 실패",
+  MEMBER_MIX_SUCCESS: "그룹 섞기 성공",
+  MEMBER_MIX_FAIL: "그룹 섞기 실패",
+
+  /* ETC */
+  OK: "성공",
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+  INTERNAL_SERVER_ERROR: "서버 내부 오류",
+
+};
