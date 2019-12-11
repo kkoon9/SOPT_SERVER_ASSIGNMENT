fa43770b80f70ec62b3917dc6f4429d2b140dd42
kkoon9 <rndrnjs2003@naver.com>
2019-12-11 14:58:59 +0900

    UPDATE route - index.js -  add blogs
    Add blogs to index(routes)

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 router.use('/groups', require('./groups'));
 router.use('/users', require('./users'));
-
+router.use('/blogs', require('./blogs'));
 module.exports = router;
