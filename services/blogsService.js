5dd9252316ed0369ab13f22b767445fa157642de
kkoon9 <rndrnjs2003@naver.com>
2019-12-16 16:30:42 +0900

    Add WEEK3 ASSIGNMENT blogService
    blog CRUD 구현

---
+++ services/blogsService.js
@@ -0,0 +1,136 @@
+const rm = require('../module/util/responseMessage');
+const utils = require('../module/util/utils');
+const sc = require('../module/util/statusCode');
+
+/**
+ * models을 가져오려면 index를 가져와야한다. blogModel를 가져오면 안된다!
+ */
+const {Blog} = require('../models');
+
+module.exports = {
+    readAll: () => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.findAll({});
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_READ_ALL_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_READ_ALL_SUCCESS, blog)
+            });
+        });
+    },
+    readblogIdx: ({
+        blogIdx
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.findOne({
+                where: {
+                    blogIdx : blogIdx,
+                }
+            });
+            if(blog.length == 0) {
+                resolve({
+                    json: utils.successFalse(sc.NO_CONTENT, rm.BLOG_READ_BLOGIDX_FAIL)
+                });
+                return;
+            }
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_READ_BLOGIDX_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_READ_BLOGIDX_SUCCESS, blog)
+            });
+        });
+    },
+    readHost: ({
+        host
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.findAll({
+                where: {
+                    host : host
+                }
+            });
+            if(blog.length == 0) {
+                resolve({
+                    json: utils.successFalse(sc.NO_CONTENT, rm.BLOG_READ_HOST_FAIL)
+                });
+                return;
+            }
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_READ_HOST_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.CREATED, rm.BLOG_READ_HOST_SUCCESS, blog)
+            });
+        });
+    },
+    create: ({
+        host,
+        introduce
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            let blog;
+            try {
+                blog = await Blog.create({
+                        host: host,
+                        introduce: introduce,
+                });
+                
+            } catch (error) {
+                reject({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_CREATE_FAIL)
+                });
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_CREATE_SUCCESS, blog)
+            });
+        });
+    },
+    update: ({
+        blogIdx,
+        introduce
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.update({
+                introduce : introduce
+            }, {
+                where : { blogIdx : blogIdx },
+            });
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_UPDATE_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_UPDATE_SUCCESS)
+            });
+        });
+    },
+    delete: ({blogIdx}) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.delete({
+                where : { blogIdx : blogIdx },
+            });
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_UPDATE_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_UPDATE_SUCCESS)
+            });
+        });
+    },
+};
