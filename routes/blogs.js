16ad669cf2766153629ff76bb22bd026ed99e1ad
kkoon9 <rndrnjs2003@naver.com>
2019-12-16 21:14:09 +0900

    Update WEEK3 ASSIGNMENT routes
    routes 이름을 바꿨다.

--- routes/blogs.js
+++ routes/blogs.js
@@ -6,21 +6,21 @@ const CommentController = require('../controllers/commentController');
 
 /* Blog */
 router.get('/', BlogController.readAll);
-router.get('/blogIdx', BlogController.readblogIdx);
+router.get('/blogId', BlogController.readblogId);
 router.get('/host', BlogController.readHost);
 router.post('/',BlogController.create);
 router.put('/',BlogController.update);
 router.delete('/',BlogController.delete);
 
-// /* Article */
-// router.get('/articles', ArticleController.readAll);
-// router.get('/:blogIdx/articles', ArticleController.read);
-// router.post('/:blogIdx/articles', ArticleController.create);
-// router.put('/:blogIdx/articles', ArticleController.update);
-// router.delete('/:blogIdx/articles', ArticleController.delete);
+/* Article */
+router.get('/articles', ArticleController.readAll);
+router.get('/:blogId/articles', ArticleController.read);
+router.post('/:blogId/articles', ArticleController.create);
+router.put('/articles', ArticleController.update);
+router.delete('/articles', ArticleController.delete);
 
-// /* Comment */
-// router.get('/articles/comments', CommentController.readAll);
+// /* Hashtag */
+// router.get('/Hashtag', CommentController.readAll);
 // router.get('/articles/:articleIdx/comments', CommentController.read);
 // router.post('/articles/:articleIdx/comments', CommentController.create);
 // router.put('/articles/:articleIdx/comments', CommentController.update);
