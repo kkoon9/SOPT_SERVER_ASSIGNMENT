1112988d18d34a55eb54e519bc4e631b435cb218
kkoon9 <rndrnjs2003@naver.com>
2019-12-17 16:50:38 +0900

    Add WEEK3 ASSIGNMENT commentModel
    commentModel CRUD 구현

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,19 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Comment', {
         nick: {
-            field: 'nick',
             type: DataTypes.STRING(40),
             allowNull: false,
-            unique: true,
         },
         content: {
-            field: 'contnet',
-            type: DataTypes.STRING(30),
+            type: DataTypes.STRING(100),
             allowNull: false,
         },
-     }, {
+    }, {
         freezeTableName: true,
-        timestamps: false,
-    
+        timestamps: true,
     });
 };
