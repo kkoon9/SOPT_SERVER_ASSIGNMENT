b8b13e001bdfbc1ac274a327efcceee687012e76
kkoon9 <rndrnjs2003@naver.com>
2019-12-20 19:58:11 +0900

    Modify WEEK5 ASSIGNMENT routes
    rename users to auth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/groups', require('./groups'));
-router.use('/users', require('./users'));
+router.use('/auth', require('./auth'));
 router.use('/blogs', require('./blogs'));
 module.exports = router;
